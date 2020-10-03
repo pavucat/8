06fa5c9cbbc63966792ff3deaab1bb5b6001a5c7
FoPQer <iritskiy.sas@gmail.com>
2020-10-03 15:18:45 +0300

    111

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,43 +1,87 @@
 package com.company;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-class shape extends JComponent
+abstract class shape extends JComponent
 {
+    Random r = new Random();
+    public Color randomcolor()
+    {
+        int a;
+        a=r.nextInt(6);
+        if(a==1)
+            return Color.red;
+        if (a==2)
+            return Color.BLACK;
+        if(a==3)
+            return Color.BLUE;
+        if(a==4)
+            return Color.CYAN;
+        if(a==5)
+            return Color.DARK_GRAY;
+        if(a==6)
+            return Color.GREEN;
+        return Color.GREEN;
+    }
     static final long serialVersionUID=1L;
     Color color;
-    shape(Color color)
+    shape()
     {
-        this.color=color;
+        this.color=randomcolor();
     }
     public void paint(Graphics g){}
+
 }
 class rectangle extends shape
 {
-    Random r = new Random();
-    rectangle(Color color)
+    rectangle()
     {
-        super(color);
+        super();
     }
 
     @Override
     public void paint(Graphics g) {
-        g.setColor(color);
-        g.fillRect(0, 0,r.nextInt(50) , r.nextInt(50));
+        g.setColor(randomcolor());
+        g.fillRect(0, 0,20 , 30);
     }
 }
-class circle extends  shape
+class square extends rectangle
 {
-    Random r = new Random();
-    circle(Color color) {
-        super(color);
+    square()
+    {
+        super();
     }
 
     @Override
     public void paint(Graphics g) {
-        g.setColor(color);
-        g.fillOval(50, 50, r.nextInt(50), r.nextInt(50));
+        g.setColor(randomcolor());
+        g.fillRect(0, 0,20 , 20);
+    }
+}
+class oval extends  shape
+{
+    Random r = new Random();
+    oval() {
+        super();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(randomcolor());
+        g.fillOval(0, 0,20, 30);
+    }
+}
+class circle extends oval
+{
+    circle()
+    {
+        super();
+    }
+    public void paint(Graphics g) {
+        g.setColor(randomcolor());
+        g.fillOval(0, 0,20, 20);
     }
 }
 public class Main extends JFrame {
@@ -47,10 +91,9 @@ public class Main extends JFrame {
         int x2=0;
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JLayeredPane lp = getLayeredPane();
-        rectangle one = new rectangle(Color.red);
-        circle two = new circle(Color.red);
-        one.setBounds(10, 40, 120, 120);
-        two.setBounds(60, 120, 160, 180);
+        rectangle one = new rectangle();
+        oval two = new oval();
+        two.setBounds(50, 50, 120, 120);
         lp.add(one, JLayeredPane.POPUP_LAYER);
         lp.add(two, JLayeredPane.POPUP_LAYER);
         setSize(280, 250);
