a5299556bb3116023d755fe132e31e025299939c
FoPQer <iritskiy.sas@gmail.com>
2020-10-17 13:29:42 +0300

    111

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,17 @@
 package com.company;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.*;
 import javax.swing.*;
 
 public class Main extends JFrame {
+    ArrayList<Shape> arrayList=new ArrayList<>();
+    int x = 25;
     public Main() {
         super("Figuri");
         Random r = new Random();
-        int x = 25;
         int a;
-        ArrayList<Shape> arrayList=new ArrayList<>();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         for(int i=0; i<20; i++)
         {
@@ -19,18 +21,26 @@ public class Main extends JFrame {
            if(a==2) arrayList.add(new Square());
            if(a==3) arrayList.add(new Oval());
         }
-        for(int i=0; i<10; i++) {
-            arrayList.get(i).setBounds(x, 0, 50, 50);
-            add(arrayList.get(i));
-            x+=25;
-        }
-        x=0;
-        for(int i=10; i<20; i++)
-        {
-            arrayList.get(i).setBounds(x, 40, 50, 50);
-            add(arrayList.get(i));
-            x+=25;
-        }
+        JButton one = new JButton();
+        one.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                one.setVisible(false);
+                for(int i=0; i<10; i++) {
+                    arrayList.get(i).setBounds(x, 0, 50, 50);
+                    add(arrayList.get(i));
+                    x+=25;
+                }
+                x=0;
+                for(int i=10; i<20; i++)
+                {
+                    arrayList.get(i).setBounds(x, 40, 50, 50);
+                    add(arrayList.get(i));
+                    x+=25;
+                }
+            }
+        });
+        add(one);
         setSize(280, 250);
         setVisible(true);
     }
